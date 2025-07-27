210244675b5092ebb1cab6578174ea1f88933559
jineelgandhi427 <jineelgandhi427@gmail.com>
2025-07-27 14:26:07 +0200

    Update final_job_scraper.py

--- final_job_scraper.py
+++ final_job_scraper.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import requests
 import os
